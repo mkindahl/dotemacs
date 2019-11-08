014fdf3e133b4062b2e61bd37ebb049fdf4025de
Mats Kindahl <mats@timescale.com>
2019-11-08 09:20:24 +0100

    Adding .emacs to repository
    The `.emacs` file is added to the repository and will be installed as
    part of the install.

--- Makefile
+++ Makefile
@@ -6,5 +6,10 @@ prefix = $(HOME)/.local
 
 all: $(compiled)
 
-install: $(compiled)
+install: install-lisp install-dotemacs
+
+install-lisp: $(compiled)
 	cp $(compiled) $(prefix)/share/emacs/site-lisp
+
+install-dotemacs:
+	cp dotemacs.el $(HOME)/.emacs
---
+++ dotemacs.el
@@ -0,0 +1,62 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; System Setting
+(setq system-time-locale "C") ; Make sure that the weekdays in the
+			      ; time stamps of your Org mode files and
+			      ; in the agenda appear in English.
+
+;; Set the load path to use
+(add-to-list 'load-path "~/.local/share/emacs/site-lisp")
+
+(load-library "org-hacks")              ;Org Mode settings
+(load-library "cc-hacks")               ;C/C++ Settings
+
+;; (setq c-default-style `((c-mode . "google")
+;;                         (c++-mode . "google")
+;;                         ,@c-default-style))
+
+(global-set-key "\C-x\C-e" 'compile)
+(global-set-key "\C-x\C-n" 'next-error)
+(global-set-key "\C-x\C-p" 'previous-error)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(clang-format-style "Google")
+ '(indent-tabs-mode t)
+ '(org-agenda-files
+   (quote
+    ("~/org/bugs.org" "~/org/someday.org" "~/org/admin.org" "~/org/research.org" "~/org/meetings.org" "~/org/refactoring.org" "~/org/reviews.org" "~/org/projects.org" "~/org/refile.org")))
+ '(org-src-preserve-indentation t)
+ '(rust-format-on-save t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
